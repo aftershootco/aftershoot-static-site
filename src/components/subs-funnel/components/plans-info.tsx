51f8af229f5495a2f43796016021399e900b767c
booi-dev <booimangang@gmail.com>
2024-01-05 16:54:10 +0530

    add: sale page

--- src/components/subs-funnel/components/plans-info.tsx
+++ src/components/subs-funnel/components/plans-info.tsx
@@ -8,16 +8,19 @@ const PlansInfo = () => {
   };
 
   return (
-    <div className="bg-[#F6F6F6] rounded-[20px] p-[48px] text-black">
+    <div className="bg-[#F6F6F6] font-archivo rounded-[20px] p-[48px] text-black">
       <button> Back </button>
 
-      <div className="flex gap-4">
-        <h1>Choose your plan</h1>
+      <div className="flex gap-8 items-center md:gap-16 mt-8">
+        <h1 className="text-[34px] -tracking-[1.02px] leading-[44px]">
+          Choose your plan
+        </h1>
 
         <ToggleSwitch
           labelBefore="Monthly"
           labelAfter="Yearly"
           onToggle={handleSwitchToggle}
+          className="font-semibold translate-y-1"
         />
       </div>
     </div>

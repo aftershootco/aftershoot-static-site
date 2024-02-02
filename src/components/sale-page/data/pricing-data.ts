ed984fddac7bdcd19fbdbc8c15efdb27d996bd59
booi-dev <booimangang@gmail.com>
2024-02-02 11:55:13 +0530

    update: pricing

--- src/components/sale-page/data/pricing-data.ts
+++ src/components/sale-page/data/pricing-data.ts
@@ -1,11 +1,41 @@
 const pricingData = [
   {
     id: "1p",
+    pricingName: "Selects",
+    description: "Ideal for editing with pre-built AI Styles",
+    price: {
+      monthly: 15,
+      yearly: 10,
+      discountedMonthly: null,
+      discountedYearly: null,
+    },
+    billType: "Billed Annually",
+    highlight: false,
+    features: [
+      {
+        id: "1",
+        featureName: "Unlimited AI Culling",
+        bold: false,
+        include: true,
+      },
+      {
+        id: "2",
+        featureName: "1 Seat Multiple Logins",
+        bold: false,
+        include: true,
+      },
+    ],
+    productId: "price_1NltSzCYznn1uZfNgmm8YUFW",
+    redirectLink:
+      "https://secure.aftershoot.com/b/eVaaHq52c4dM4pO00t?prefilled_promo_code",
+  },
+  {
+    id: "2p",
     pricingName: "Edits",
     description: "Unlimited Culling at your disposal",
     price: {
-      monthly: 15,
-      yearly: null,
+      monthly: 18,
+      yearly: 15,
       discountedMonthly: null,
       discountedYearly: null,
     },
@@ -35,43 +65,14 @@ const pricingData = [
     redirectLink:
       "https://secure.aftershoot.com/b/28o2aU7ak9y68G400y?prefilled_promo_code",
   },
-  {
-    id: "2p",
-    pricingName: "Selects",
-    description: "Ideal for editing with pre-built AI Styles",
-    price: {
-      monthly: 10,
-      yearly: null,
-      discountedMonthly: null,
-      discountedYearly: null,
-    },
-    billType: "Billed Annually",
-    highlight: false,
-    features: [
-      {
-        id: "1",
-        featureName: "Unlimited AI Culling",
-        bold: false,
-        include: true,
-      },
-      {
-        id: "2",
-        featureName: "1 Seat Multiple Logins",
-        bold: false,
-        include: true,
-      },
-    ],
-    productId: "price_1NltSzCYznn1uZfNgmm8YUFW",
-    redirectLink:
-      "https://secure.aftershoot.com/b/eVaaHq52c4dM4pO00t?prefilled_promo_code",
-  },
+
   {
     id: "3p",
     pricingName: "Pro",
     description: "Edit like a Pro with Personal AI Editing Profiles",
     price: {
-      monthly: 40,
-      yearly: null,
+      monthly: 48,
+      yearly: 40,
       discountedMonthly: null,
       discountedYearly: null,
     },
@@ -124,8 +125,8 @@ const pricingData = [
     pricingName: "Max",
     description: "Ideal package for high volume photographers",
     price: {
-      monthly: 60,
-      yearly: null,
+      monthly: 72,
+      yearly: 60,
       discountedMonthly: null,
       discountedYearly: null,
     },
